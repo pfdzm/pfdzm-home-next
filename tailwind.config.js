551dc1b7858f60de731f9678f7ea89e05458d9e5
pfdzm <pfdzm@users.noreply.github.com>
2021-05-26 11:08:19 +0200

    Initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,41 @@
+const colors = require('tailwindcss/colors')
+
+module.exports = {
+  purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    colors: {
+      transparent: 'transparent',
+      current: 'currentColor',
+      blue: colors.blue,
+      rose: colors.rose,
+      white: colors.white,
+      amber: colors.amber,
+      gray: colors.warmGray,
+    },
+    fontFamily: {
+      body: [
+        'Poppins',
+        'ui-sans-serif',
+        'system-ui',
+        '-apple-system',
+        'BlinkMacSystemFont',
+        'Segoe UI',
+        'Roboto',
+        'Helvetica Neue',
+        'Arial',
+        'Noto Sans',
+        'sans-serif',
+        'Apple Color Emoji',
+        'Segoe UI Emoji',
+        'Segoe UI Symbol',
+        'Noto Color Emoji',
+      ],
+    },
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
