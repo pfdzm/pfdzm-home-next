3efdf0afa3a8014c2414191b5d2c125e89195ca1
pfdzm <pfdzm@users.noreply.github.com>
2021-05-26 13:48:21 +0200

    Fix OG errors

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -11,10 +11,16 @@ class MyDocument extends Document {
       <Html lang="en">
         <Head>
           <meta property="og:title" content="pfdzm's personal website" />
-          <meta property="og:image" content="/assets/personal-website.png" />
+          <meta
+            property="og:image"
+            content="https://user-images.githubusercontent.com/17315212/119654465-c8194280-be28-11eb-896b-daf44ce145c8.png"
+          />
           <meta
             property="og:description"
-            content="This is my personal website - enjoy :)"
+            content="Hi! I'm Pablo. I'm a Fullstack Software Engineer based in Berlin, Germany. I hail
+            from Madrid, Spain but have lived in Berlin since 2009. I currently
+            work for Enpal GmbH in the Marketing & Sales Tech (aka Growth Tech)
+            team, helping to build and scale the future of green tech."
           />
           <meta property="og:url" content="https://www.pfdzm.me/" />
         </Head>
